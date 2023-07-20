457a79ae426e7fc3bdb7b505651bd6fc2b5df05b
Dmitry Dadaka <belpc2012@gmail.com>
2023-07-20 17:54:00 +0300

    feat: added middleware featers, mongoose, mongodb

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 3500
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT
 
+connectDB()
+
+app.use(logger)
+app.use(cors(corsOptions))
 app.use(express.json())
-
+app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/route'))
+
 app.all('*', (req, res) => {
     res.status(404)
     if(req.accepts('html')){
@@ -18,6 +31,15 @@ app.all('*', (req, res) => {
     }
 })
 
+app.use(errorHandler)
 
-app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`)) 
+mongoose.connection.once('open', () => {
+    console.log('db connected')
+    app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`)) 
+})
+
+mongoose.connection.on( 'error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
 
