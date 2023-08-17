d9b3a61b6fd2621319d5dfe283a0d48b684d4751
Dmitry Dadaka <belpc2012@gmail.com>
2023-08-17 14:56:08 +0300

    feat: added dashlayout and features components

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+import DashLayout from './components/DashLayout';
 import Layout from './components/Layout'
+import Login from './features/auth/Login';
 import Public from './components/public';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={ <Layout />} >
-        <Route index element={ <Public />} />
+      <Route path='/' element={ <Layout />  } >
+      <Route index element={ <Public /> } />
+      <Route path='/login' element={ <Login /> } />
+      <Route path='/dash' element={ <DashLayout /> } />
+
       </Route> 
     </Routes>
   );
