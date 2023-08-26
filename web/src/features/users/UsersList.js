12889a55a53564f8d038cec7bdc0f0edb8d0655f
Dmitry Dadaka <belpc2012@gmail.com>
2023-08-26 23:10:17 +0300

    feat added new features

--- web/src/features/users/UsersList.js
+++ web/src/features/users/UsersList.js
@@ -1,4 +1,6 @@
-import { useGetUsersQuery } from './userApiSlice'
+import { useGetUsersQuery } from "./userApiSlice"
+import User from './User'
+
 const UsersList = () => {
 
     const {
@@ -12,15 +14,34 @@ const UsersList = () => {
     let content
 
     if (isLoading) content = <p>Loading...</p>
-    
+
     if (isError) {
-      content = <p className='errmsg'>{error?.data?.message}</p>
+        content = <p className="errmsg">{error?.data?.message}</p>
     }
 
-    if(isSuccess){
+    if (isSuccess) {
 
+        const { ids } = users
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+
+        content = (
+            <table className="table table--users">
+                <thead className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th user__username">Username</th>
+                        <th scope="col" className="table__th user__roles">Roles</th>
+                        <th scope="col" className="table__th user__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
     }
-
     return content
 }
 
