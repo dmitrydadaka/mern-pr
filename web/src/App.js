290ecb5e9110cd83ed65cd2bce41f928c777027d
Dmitry Dadaka <belpc2012@gmail.com>
2023-09-26 10:08:09 +0300

    feat: fixed errors

--- web/src/App.js
+++ web/src/App.js
@@ -10,6 +10,7 @@ import NewNote from './features/notes/NewNote'
 import EditNote from './features/notes/EditNote'
 import NewUserForm from './features/users/NewUserForm'
 import EditUser from './features/users/EditUser'
+import Prefetch from './features/auth/Prefetch'
 
 function App() {
   return (
@@ -17,17 +18,19 @@ function App() {
       <Route path='/' element={<Layout />} >
         <Route index element={<Public />} />
         <Route path='login' element={<Login />} />
-        <Route path='dash' element={<DashLayout />} >
-          <Route index element={<Welcome />} />
-          <Route path='notes'>
-            <Route index element={<NotesList />} />
-            <Route path={':id'} element={<EditNote />} /> 
-            <Route path={'new'} element={<NewNote/>} />
-          </Route>
-          <Route path='users'>
-            <Route index element={<UsersList />} />
-            <Route path={':id'} element={<EditUser />} /> 
-            <Route path={'new'} element={<NewUserForm/>} /> 
+        <Route element={<Prefetch />}>
+          <Route path='dash' element={<DashLayout />} >
+            <Route index element={<Welcome />} />
+            <Route path='notes'>
+              <Route index element={<NotesList />} />
+              <Route path={':id'} element={<EditNote />} />
+              <Route path={'new'} element={<NewNote />} />
+            </Route>
+            <Route path='users'>
+              <Route index element={<UsersList />} />
+              <Route path={':id'} element={<EditUser />} />
+              <Route path={'new'} element={<NewUserForm />} />
+            </Route>
           </Route>
         </Route>
       </Route>
--- web/src/features/users/NewUserForm.js
+++ web/src/features/users/NewUserForm.js
@@ -26,13 +26,13 @@ const NewUserForm = () => {
     const [roles, setRoles] = useState(["Employee"])
 
     useEffect(() => {
-       setValidUsername(USER_REGEX.test(username))}, 
-       [username]
-    )
+        setValidUsername(USER_REGEX.test(username))
+    }, [username])
+
     useEffect(() => {
-        setValidPassword(PWD_REGEX.test(username))}, 
-        [password]
-    )
+        setValidPassword(PWD_REGEX.test(password))
+    }, [password])
+
     useEffect(() => {
         if (isSuccess) {
             setUsername('')
@@ -40,73 +40,85 @@ const NewUserForm = () => {
             setRoles([])
             navigate('/dash/users')
         }
-    }, [isSuccess, navigate]
-    )
-    const onUsernameChanged = (e) => setUsername(e.target.value)
-    const onPasswordChanged = (e) => setPassword(e.target.value)
-    const onRolesChanged = (e) => {
+    }, [isSuccess, navigate])
+
+    const onUsernameChanged = e => setUsername(e.target.value)
+    const onPasswordChanged = e => setPassword(e.target.value)
+
+    const onRolesChanged = e => {
         const values = Array.from(
-            e.target.selectedOptions,
+            e.target.selectedOptions, //HTMLCollection 
             (option) => option.value
         )
         setRoles(values)
     }
+
     const canSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoading
+
     const onSaveUserClicked = async (e) => {
         e.preventDefault()
         if (canSave) {
             await addNewUser({ username, password, roles })
         }
     }
+
     const options = Object.values(ROLES).map(role => {
         return (
-            <option key={role} value={role}>{role}</option>
+            <option
+                key={role}
+                value={role}
+
+            > {role}</option >
         )
     })
-    const errClass = isError ? 'errmsg' : 'offscreen'
-    const validUserClass = !validUsername ? 'form-input--incomplete' : ''
-    const validPwdClass = !validPassword ? 'form-input--incomplete' : ''
-    const validRolesClass = !Boolean(roles.length) ? 'form-input--incomplete' : ''
+
+    const errClass = isError ? "errmsg" : "offscreen"
+    const validUserClass = !validUsername ? 'form__input--incomplete' : ''
+    const validPwdClass = !validPassword ? 'form__input--incomplete' : ''
+    const validRolesClass = !Boolean(roles.length) ? 'form__input--incomplete' : ''
+
 
     const content = (
         <>
             <p className={errClass}>{error?.data?.message}</p>
+
             <form className="form" onSubmit={onSaveUserClicked}>
-                <div className='form__title-row'>
+                <div className="form__title-row">
                     <h2>New User</h2>
-                    <div className='form__action-buttons'>
-                        <button className='icon-button'
-                        title='Save'
-                        disabled={!canSave}
+                    <div className="form__action-buttons">
+                        <button
+                            className="icon-button"
+                            title="Save"
+                            disabled={!canSave}
                         >
                             <FontAwesomeIcon icon={faSave} />
                         </button>
                     </div>
                 </div>
-                <label className='form__label' htmlFor='username'>
-                    Username: <span className='nowrap'>[3-20 letters]</span>
-                </label>
-                <input 
-                className={`form__input ${validUserClass}`}
-                id='username'
-                name='username'
-                type='text'
-                autocomplete='off'
-                value={username}
-                onChange={onUsernameChanged}
+                <label className="form__label" htmlFor="username">
+                    Username: <span className="nowrap">[3-20 letters]</span></label>
+                <input
+                    className={`form__input ${validUserClass}`}
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="off"
+                    value={username}
+                    onChange={onUsernameChanged}
                 />
-                <label className='form__label' htmlFor='password'>
-                    Username: <span className='nowrap'>[4-12 chars incl. !@#$%]</span>
-                </label>
-                <input 
-                className={`form__input ${validPwdClass}`}
-                id='password'
-                name='password'
-                type='password'
-                value={password}
-                onChange={onPasswordChanged}
+
+                <label className="form__label" htmlFor="password">
+                    Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span></label>
+                <input
+                    className={`form__input ${validPwdClass}`}
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={onPasswordChanged}
                 />
-                    <label className="form__label" htmlFor="roles">
+
+                <label className="form__label" htmlFor="roles">
                     ASSIGNED ROLES:</label>
                 <select
                     id="roles"
@@ -115,15 +127,14 @@ const NewUserForm = () => {
                     multiple={true}
                     size="3"
                     value={roles}
-             
                     onChange={onRolesChanged}
                 >
                     {options}
                 </select>
-            </form>       
+
+            </form>
         </>
     )
-
     return content
 }
 
