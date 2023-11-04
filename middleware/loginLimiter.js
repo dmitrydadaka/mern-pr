efe30fea30bc993f6ed3d238e5431d856ae6b8fa
Dmitry Dadaka <belpc2012@gmail.com>
2023-11-04 20:25:31 +0300

    feat: added loginlimiter

---
+++ middleware/loginLimiter.js
@@ -0,0 +1,19 @@
+const rateLimit = require('express-rate-limit')
+const { logEvents } = require('.logger')
+
+const  loginLimiter = rateLimit({
+    windowMs: 60*1000,
+    max: 5,
+    message: {
+        message: 'Too many login attempts from that IP, please try again after a 60 second pause'
+    },
+    handler: (req,res, next, options) => {
+        logEvents(`Too many requests: ${options.message.message}\t${req.method}\t${req.url}
+        \t${req.headers.origin}`, 'errLog.log')
+        res.status(options.statusCode).send(options.message)
+    },
+    standardHeaders: true,
+    legaceHeaders: false
+})
+
+module.exports = loginLimiter
